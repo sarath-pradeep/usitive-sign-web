725a21bbcd26ea6b308b4f97fb72e22aa9c020e9
Sarath Pradeep <sarath@finesseink.com>
2025-07-18 11:02:11 +0530

    base url change for render

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://usitive-sign-server.onrender.com/api';
 
 export const loginUser = async (email, password) => {
   try {
